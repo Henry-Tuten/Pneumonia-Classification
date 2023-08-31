5a061f57f46a7b0604bcafd3c77e7ff5c40d396d
Henry Tuten <jatu5220@colorado.edu>
2023-08-31 08:04:38 -0600

    add stuff

--- src/training/pytorch/train_cnn1.py
+++ src/training/pytorch/train_cnn1.py
@@ -1,8 +1,16 @@
 import torch
 import torch.nn as nn
 import transformers
+import sys
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
+from src.models.architecture.pytorch import pyt_transf
+
+#------------------Establish Directory-------------------
+sys.path.append('src/models/architecture/pytorch/pyt_transf,py')
+
+#--------------------------------------------------------
+
 
 #------------------Hyper Parameters----------------------
 batch_size = 32
@@ -21,6 +29,16 @@ else:
 
 #--------------------------------------------------------
 
+#------------------Transformations-----------------------
+
+# Establish transformation
+
+transform =
+
+
+
+#--------------------------------------------------------
+
 #------------------Data_Loader---------------------------
 
 train_dataset = datasets.Imagefolder(root= "data/train/", transform=transform)
@@ -32,5 +50,19 @@ dataloader= DataLoader(train_dataset, batch_size, shuffle=True)
 #------------------Instantiate Model---------------------
 
 model = pyt_transf()
+
+#--------------------------------------------------------
+
+
+#------------------Training Loop-------------------------
+
+
+# Consider using multiple epochs then looping within 
+# for dataloader
+for instance in dataloader:
+
+
+    # Set gradients to zero for each iteration
+    torch.no_grad()
 
 #--------------------------------------------------------
